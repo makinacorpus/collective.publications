92de81879a7018e0d66f90640d48068539a4c211
Emmanuelle Helly <emmanuelle.helly@gmail.com>
2013-01-10 23:38:24 +0100

    Upgrade: add CSS in static resource.

---
+++ collective/publications/migration/upgrades.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+import logging
+logger = logging.getLogger("GenericSetup")
+
+try:
+    from Products.CMFPlone.migrations import migration_util
+except:
+    #plone4
+    from plone.app.upgrade import utils as migration_util
+
+from Products.CMFCore.utils import getToolByName
+from Products.ATContentTypes.interface.image import IATImage
+from Products.ATContentTypes.content.image import ATImage
+from collective.publications.content.publication import IPublication
+from plone.dexterity.interfaces import IDexterityContent
+import transaction
+
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import _createObjectByType
+
+from logging import getLogger
+logger = getLogger('collective.publications.migration')
+#logger.info(message)
+
+def upgrade_to_1001(context):
+    """Upgrade to 1001"""
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile('profile-collective.publications:default', 'cssregistry', run_dependencies=False)
+    setup.runImportStepFromProfile('profile-collective.publications:default', 'typeinfo', run_dependencies=False)
+    transaction.commit()
+    
+    
+
