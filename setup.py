f7cab85493c6755d2c9e7e9427bbe2ec3b8f4ae6
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2013-02-05 10:15:20 +0100

    Remove keywordwidget.

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(name='collective.publications',
             'plone.namedfile [blobs]',
             'collective.dexteritytextindexer',
             'collective.z3cform.widgets',
-            'collective.z3cform.keywordwidget',
             'collective.z3cform.datetimewidget',
             'plone.app.referenceablebehavior',
             # -*- Extra requirements: -*-
