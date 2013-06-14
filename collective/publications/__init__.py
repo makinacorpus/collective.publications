c2bd56c7c0601e72411a6e81339e47feca5b6300
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2013-06-14 17:29:33 +0200

    up

--- collective/publications/__init__.py
+++ collective/publications/__init__.py
@@ -5,13 +5,11 @@ MessageFactory = MessageFactory('collective.publications')
 
 # Override subjects fields in dexterity contents
 # (see http://pypi.python.org/pypi/collective.z3cform.widgets#override-existing-fields)
-
-from plone.autoform.interfaces import WIDGETS_KEY
-from plone.directives.form.schema import TEMP_KEY
 from plone.app.dexterity.behaviors.metadata import ICategorization
 from zope import schema as _schema
 
-_directives_values = ICategorization.queryTaggedValue(TEMP_KEY)
-_directives_values.setdefault(WIDGETS_KEY, {})
-_directives_values[WIDGETS_KEY]['subjects'] = 'collective.z3cform.widgets.token_input_widget.TokenInputFieldWidget'
+
+from plone.directives import form
+
+form.widget(ICategorization['subjects'], 'collective.z3cform.widgets.token_input_widget.TokenInputFieldWidget')
 _schema.getFields(ICategorization)['subjects'].index_name = 'Subject' 
